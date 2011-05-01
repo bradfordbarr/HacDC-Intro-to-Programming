014c8f1875c117d68749e05cc8624422260becea
Bradford Barr <bradford.barr@gmail.com>
2011-05-01 14:13:52 -0400

    Adding some stuff for the 0th class.

---
+++ class_0/wrapper_turtle.py
@@ -0,0 +1,21 @@
+import turtle
+
+T = turtle.Turtle()
+
+def forward(n):
+	T.forward(n)
+
+def backward(n):
+	T.backward(n)
+
+def left(n):
+	T.left(n)
+
+def right(n):
+	T.right(n)
+
+def penup():
+	T.penup()
+
+def pendown():
+	T.pendown()
