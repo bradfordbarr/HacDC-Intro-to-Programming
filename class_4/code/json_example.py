e9d4d62cd2849b114a5f8581879c073ea24bfc47
Bradford Barr <bradford.barr@gmail.com>
2011-06-16 19:03:17 -0400

    Updated the json example.

--- class_4/code/json_example.py
+++ class_4/code/json_example.py
@@ -3,6 +3,25 @@ import urllib2
 
 URL = "http://bitly.measuredvoice.com/bitly_archive/usagov_bitly_data2011-06-16-1308257921"
 
+# This code downloads data for an upcoming hackathon
+# It's format goes a little something like this:
+#{
+#	"a": USER_AGENT,
+#	"c": COUNTRY_CODE, # 2-character iso code
+#	"nk": KNOWN_USER,  # 1 or 0. 0=this is the first time we've seen this browser
+#	"g": GLOBAL_BITLY_HASH,
+#	"h": ENCODING_USER_BITLY_HASH,
+#	"l": ENCODING_USER_LOGIN,
+#	"hh": SHORT_URL_CNAME,
+#	"r": REFERRING_URL,
+#	"u": LONG_URL,
+#	"t": TIMESTAMP,
+#	"gr": GEO_REGION,
+#	"ll": [LATITUDE, LONGITUDE],
+#	"cy": GEO_CITY_NAME,
+#	"tz": TIMEZONE # in http://en.wikipedia.org/wiki/Zoneinfo format
+#} 
+
 def main():
 	page_file = urllib2.urlopen(URL)
 	data = []
